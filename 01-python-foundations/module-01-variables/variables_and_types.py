3d214878bc1914057938f3d28016698a87286943
arvinddevarajan <arvinddevarajan16@gmail.com>
2026-05-14 22:30:10 +0000

    learn: module 01 - variables and types

---
+++ 01-python-foundations/module-01-variables/variables_and_types.py
@@ -0,0 +1,24 @@
+name ="Alice"
+
+print(name)
+
+age = 30
+height = 5.9
+is_engineer = True
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_engineer))
+
+age = "thirty"
+print(age)
+print(type(age))
+
+score = "95"
+print(type(score))
+
+score = int(score)
+print(type(score))
+print(score + 5)
